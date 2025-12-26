cb8c41e967f0f9539aece628a01c192606562eee
NachArico <ignacio.arico11@gmail.com>
2025-12-26 13:02:36 +0100

    feat: ocultar credenciales de Firebase usando variables de entorno

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDg-mzC_WsTDD2lgv0ghpMxV2a3AZ-AUow",
-  authDomain: "batcavecryptoignacioarico.firebaseapp.com",
-  projectId: "batcavecryptoignacioarico",
-  storageBucket: "batcavecryptoignacioarico.firebasestorage.app",
-  messagingSenderId: "623615913522",
-  appId: "1:623615913522:web:587e6eb009ba620b2d562b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
